5658f1d91371be6a884f0dc4d6900532d65f3e55
alexmking <alexking921@yahoo.com>
2025-04-14 13:51:58 -0500

    Update changes

--- expl.py
+++ expl.py
@@ -1,2 +1,3 @@
 # niche_side_quests_repo
 # storing all my shortcuts / config files / settings / tuts / etc
+# priority on universal key bindings across primarily windows and mac os (linux is a bonus)
