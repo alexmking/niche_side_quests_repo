598d381f46f9bb20d7bd08d974fb1e4fa2f17f7c
King, Alex <alex.king@osii.com>
2026-04-16 15:31:14 -0500

    adding lookup scripts3

---
+++ win/lookup-keybvis-win.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Lookup KeybVis-win
+# @raycast.mode fullOutput
+
+# Optional parameters:
+# @raycast.icon 🤖
+# @raycast.packageName Developer Utils
+# @raycast.description lookup for keyboard visualizer html
+# @raycast.argument1 { "type": "text", "placeholder": "Placeholder" }
+
+# Documentation:
+# @raycast.author alexmking921
+# @raycast.authorURL https://raycast.com/alexmking921
+
+echo "Hello World! Argument1 value: $1"
