e18524d4dcb75a3f922d64149ae4ded2ee6f21c8
alexmking <alexking921@yahoo.com>
2026-04-16 12:45:00 -0500

    added edit command for testing launching it from raycast context

---
+++ mac/edit-ref.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Edit Ref
+# @raycast.mode compact
+
+# Optional parameters:
+# @raycast.icon 🤖
+# @raycast.packageName Developer Utils
+
+# Documentation:
+# @raycast.description opens ssot-ref doc in vscode
+# @raycast.author alexmking921
+# @raycast.authorURL https://raycast.com/alexmking921
+
+echo "Hello World!"
+
