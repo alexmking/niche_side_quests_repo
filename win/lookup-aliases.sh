b54ef7812d9143042153d9c578072958bf5e9f2f
King, Alex <alex.king@osii.com>
2026-04-16 12:46:20 -0500

    adding lookup scripts2

--- win/lookup-aliases.sh
+++ win/lookup-aliases.sh
@@ -1,16 +1,35 @@
-#!/bin/bash
+# !/bin/bash
 
 # Required parameters:
 # @raycast.schemaVersion 1
 # @raycast.title Lookup Aliases
 # @raycast.mode fullOutput
-
-# Optional parameters:
+# @raycast.argument1 { "type": "text", "placeholder": "Lookup Aliases..." }
 # @raycast.icon 🤖
-# @raycast.argument1 { "type": "text", "placeholder": "Placeholder" }
 
 # Documentation:
 # @raycast.author alexmking921
 # @raycast.authorURL https://raycast.com/alexmking921
 
-echo "Hello World! Argument1 value: $1"
+# FILE="$HOME/ssot-refs.txt"
+# /usr/local/bin/alexScripts/raycast_scripts/ssot-refs.txt
+# FILE="/usr/local/bin/alexScripts/raycast_scripts/ssot-refs.txt"
+# FILE="/Users/alexking/local_docs/GIT_STUFF/niche_side_quests_repo/_universal/keyboard-shortcuts.html"
+# FILE="/Users/alexking/local_docs/GIT_STUFF/niche_side_quests_repo/mac/.zshrc"
+FILE="C:\Users\aking\Downloads\cmder\config\user_aliases.cmd"
+
+QUERY="$1"
+
+if [ -z "$QUERY" ]; then
+  head -n 15 "$FILE"
+  exit 0
+fi
+
+PRIMARY=$(rg -i "^$QUERY\b" "$FILE")
+
+if [ -n "$PRIMARY" ]; then
+  echo "$PRIMARY"
+  exit 0
+fi
+
+rg -i "$QUERY" "$FILE" | head -n 20
