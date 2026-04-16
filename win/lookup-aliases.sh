2f5625aeea626e7a54ce4f97ffd53eaaf8d38b38
King, Alex <alex.king@osii.com>
2026-04-16 10:52:53 -0500

    adding lookup scripts

---
+++ win/lookup-aliases.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Lookup Aliases
+# @raycast.mode fullOutput
+
+# Optional parameters:
+# @raycast.icon 🤖
+# @raycast.argument1 { "type": "text", "placeholder": "Placeholder" }
+
+# Documentation:
+# @raycast.author alexmking921
+# @raycast.authorURL https://raycast.com/alexmking921
+
+echo "Hello World! Argument1 value: $1"
---
+++ win/lookup-keybvis.bat
@@ -0,0 +1,16 @@
+@echo off
+
+REM Required parameters:
+REM @raycast.schemaVersion 1
+REM @raycast.title Lookup KeybVis
+REM @raycast.mode fullOutput
+
+REM Optional parameters:
+REM @raycast.icon 🤖
+REM @raycast.argument1 { "type": "text", "placeholder": "Placeholder" }
+
+REM Documentation:
+REM @raycast.author alexmking921
+REM @raycast.authorURL https://raycast.com/alexmking921
+
+echo Hello World! Argument1 value: %1
