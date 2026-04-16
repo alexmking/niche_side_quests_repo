7baefc1e29cda6863b1c413b27fc461d7f8bcffa
King, Alex <alex.king@osii.com>
2026-04-16 15:35:14 -0500

    adding lookup scripts3

--- win/lookup-keybvis-win.sh
+++ win/lookup-keybvis-win.sh
@@ -1,18 +1,36 @@
-#!/bin/bash
+# !/bin/bash
 
 # Required parameters:
 # @raycast.schemaVersion 1
-# @raycast.title Lookup KeybVis-win
+# @raycast.title Lookup KeybVis
 # @raycast.mode fullOutput
-
-# Optional parameters:
+# @raycast.argument1 { "type": "text", "placeholder": "Lookup KeybVis..." }
 # @raycast.icon 🤖
-# @raycast.packageName Developer Utils
-# @raycast.description lookup for keyboard visualizer html
-# @raycast.argument1 { "type": "text", "placeholder": "Placeholder" }
 
 # Documentation:
 # @raycast.author alexmking921
 # @raycast.authorURL https://raycast.com/alexmking921
 
-echo "Hello World! Argument1 value: $1"
+# FILE="$HOME/ssot-refs.txt"
+# /usr/local/bin/alexScripts/raycast_scripts/ssot-refs.txt
+# FILE="/usr/local/bin/alexScripts/raycast_scripts/ssot-refs.txt"
+# FILE="/Users/alexking/local_docs/GIT_STUFF/niche_side_quests_repo/_universal/keyboard-shortcuts.html"
+# FILE="/Users/alexking/local_docs/GIT_STUFF/niche_side_quests_repo/mac/.zshrc"
+# FILE="C:\Users\aking\Downloads\cmder\config\user_aliases.cmd"
+FILE="D:\git\niche_side_quests_repo\_universal\keyboard-shortcuts.html"
+
+QUERY="$1"
+
+if [ -z "$QUERY" ]; then
+  head -n 15 "$FILE"
+  exit 0
+fi
+
+PRIMARY=$(rg -i "^$QUERY\b" "$FILE")
+
+if [ -n "$PRIMARY" ]; then
+  echo "$PRIMARY"
+  exit 0
+fi
+
+rg -i "$QUERY" "$FILE" | head -n 20
