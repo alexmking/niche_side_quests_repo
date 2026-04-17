a3a407696d6913db33d757bf9b0d874c96816546
alexmking <alexking921@yahoo.com>
2026-04-17 00:17:23 -0500

    update with many new script-commands

---
+++ mac/advanced-finder.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Advanced Finder
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon 🤖
+
+# Documentation:
+# @raycast.description opens a new finder window at the selected location (SELECTION via fuzzyfinding/dropdown mech) similar to my Advanced-Open script command idea(s)...not sure on specifics for this one just yet as it could be kinda redundant??
+# @raycast.author alexmking921
+# @raycast.authorURL https://raycast.com/alexmking921
+
+echo "Hello World!"
+
+# OTHER SIMILAR IDEAS / ENHANCEMENTS:
+# 1. open fileExpl window at selected location, but the selection is fuzzyFind/dropdown across a hard-coded collection of my most common/useful directory spots
+# 2. a lot of similar ideas that all revolve around selecting via fuzzyFinding/dropdown from a predefined list ofmy most useful apps, files, directories, or paths
+# 3. this 'selection via fuzzyFinding/dropdown' approach is insanely powerful and expandable/flexible, so most of my curr ideas for script-commands are about leveraging this mechanism to streamline access to frequently used resources
+# 4.    - open terminal at SELECTED location (via fuzzyFinding/dropdown as the selection mechanism)
+#       - copy path that's SELECTED (via fuzzyFinding/dropdown as the selection mechanism)
+#       - open file in VS Code that's SELECTED (via fuzzyFinding/dropdown as the selection mechanism)
+#
+#
+#
+#
+#
+#
+#
+#
---
+++ mac/advanced-open.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Advanced Open
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon 🤖
+
+# Documentation:
+# @raycast.description Fuzzy find from collection of my most-used apps, files, directories , paths etc and then open 
+# @raycast.author alexmking921
+# @raycast.authorURL https://raycast.com/alexmking921
+
+echo "Hello World!"
+
+# OTHER SIMILAR IDEAS / ENHANCEMENTS:
+# 1. open fileExpl window at selected location, but the selection is fuzzyFind/dropdown across a hard-coded collection of my most common/useful directory spots
+# 2. a lot of similar ideas that all revolve around selecting via fuzzyFinding/dropdown from a predefined list ofmy most useful apps, files, directories, or paths
+# 3. this 'selection via fuzzyFinding/dropdown' approach is insanely powerful and expandable/flexible, so most of my curr ideas for script-commands are about leveraging this mechanism to streamline access to frequently used resources
+# 4.    - open terminal at SELECTED location (via fuzzyFinding/dropdown as the selection mechanism)
+#       - copy path that's SELECTED (via fuzzyFinding/dropdown as the selection mechanism)
+#       - open file in VS Code that's SELECTED (via fuzzyFinding/dropdown as the selection mechanism)
+#
+#
+#
+#
+#
+#
+#
+#
