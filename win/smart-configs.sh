196a0eeb08bcfb9ce0e3b7aad5af830e5280e00a
King, Alex <alex.king@osii.com>
2026-04-20 14:34:32 -0500

    update2

---
+++ win/smart-configs.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Smart Configs
+# @raycast.mode compact
+
+# Optional parameters:
+# @raycast.icon 🤖
+# @raycast.packageName Developer Utils
+# @raycast.description Similar to Advanced-Finder, it's a fzf/dropdown of all our main config files (or at least the ones we'd ever have to edit/look-at)
+
+# Documentation:
+# @raycast.author alexmking921
+# @raycast.authorURL https://raycast.com/alexmking921
+
+echo "Hello World!"
