9be6a788c8753c2884a9fcdaf4ac9445a9a3e913
alexmking <alexking921@yahoo.com>
2026-04-20 14:34:49 -0500

    updates

---
+++ mac/morning-prep-(via-rayc)(beta).py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title morning prep (via rayc)(beta)
+# @raycast.mode fullOutput
+
+# Optional parameters:
+# @raycast.icon 🤖
+# @raycast.argument1 { "type": "text", "placeholder": "Placeholder" }
+# @raycast.argument2 { "type": "text", "placeholder": "Placeholder" }
+
+# @raycast.packageName Developer Utils
+
+# Documentation:
+# @raycast.description run morningprep leet script via raycast (attempt 1)
+# @raycast.author alexmking921
+# @raycast.authorURL https://raycast.com/alexmking921
+
+# from curses import echo
+import os
+import sys
+# print("Hello World! Argument1 value: " + sys.argv[1])
+# print("Hello World! Argument2 value: " + sys.argv[2])
+
+# echo "/Users/alexking/amk/tools/alexScripts/scr-myScript.py"
+
+# /Users/alexking/amk/tools/alexScripts/scr-print-userInput_TEST.py
+
+
+# /Users/alexking/amk/tools/alexScripts/scr-myScript.py
+
+
+# /Users/alexking/amk/tools/GIT_STUFF/niche_side_quests_repo/mac/scr-leet_morning_random.py
+
+# os.system("python script.py")
+
+
+
+
+# os.system("/Users/alexking/amk/tools/GIT_STUFF/niche_side_quests_repo/mac/scr-leet_morning_random.py any any")
+
+os.system("/Users/alexking/amk/tools/GIT_STUFF/niche_side_quests_repo/mac/scr-leet_morning_random.py " + sys.argv[1] + " " + sys.argv[2])
+
+
+
+
+
+
+
