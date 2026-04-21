1d6972232e144646d115f8428deb342747ef27cc
King, Alex <alex.king@osii.com>
2026-04-20 21:47:07 -0500

    adding smart dropdownfzf

---
+++ win/config-dropdown1.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python3
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Config Dropdown1
+# @raycast.mode compact
+# @raycast.icon 🤖
+# @raycast.packageName Developer Utils
+# @raycast.description trying fzf/dropdown for all configs
+
+# @raycast.argument1 { "type": "dropdown", "placeholder": "sel config", "required": true, "data": [{"title": "aliases", "value": "A"}, {"title": "ahk", "value": "H"}, {"title": "glaze", "value": "G"}, {"title": "flexprompt", "value": "P"}, {"title": "fzf", "value": "F"}, {"title": "keybVis", "value": "K"}, {"title": "ssot-refs", "value": "R"}, {"title": "madden", "value": "M"} ] }
+
+
+
+
+
+# C:\Users\aking\Downloads\cmder\config\user_aliases.cmd
+# C:\Users\aking\.glzr\glazewm\config.yaml
+# C:\Users\aking\AppData\Local\Programs\AutoHotkey\UX\king_app_switcher1.ahk
+# C:\Users\aking\Downloads\cmder\config\flexprompt_config.lua
+# C:\Users\aking\Downloads\cmder\vendor\clink\clink_gizmos\fzf.lua
+# code D:\git\niche_side_quests_repo\_universal\keyboard-shortcuts.html
+# D:\git\niche_side_quests_repo\_universal\ssot-refs.md
+
+# Documentation:
+# @raycast.author alexmking921
+# @raycast.authorURL https://raycast.com/alexmking921
+import os
+import sys
+# print("Hello World! Argument1 value: " + sys.argv[1])
+selection = sys.argv[1]
+
+# os.system(r"Code C:\Users\aking\Downloads\cmder\config\user_aliases.cmd")
+# A H G P F K R M
+if selection == "A":
+    os.system(r"Code C:\Users\aking\Downloads\cmder\config\user_aliases.cmd")
+elif selection == "H":
+    os.system(r"Code C:\Users\aking\AppData\Local\Programs\AutoHotkey\UX\king_app_switcher1.ahk")
+elif selection == "G":
+    os.system(r"Code C:\Users\aking\.glzr\glazewm\config.yaml")
+elif selection == "P":
+    os.system(r"Code C:\Users\aking\Downloads\cmder\config\flexprompt_config.lua")
+elif selection == "F":
+    os.system(r"Code C:\Users\aking\Downloads\cmder\vendor\clink\clink_gizmos\fzf.lua")
+elif selection == "K":
+    os.system(r"Code D:\git\niche_side_quests_repo\_universal\keyboard-shortcuts.html")
+elif selection == "R":
+    os.system(r"Code D:\git\niche_side_quests_repo\_universal\ssot-refs.md")
+elif selection == "M":
+    os.system(r"Code MADDEN")
+else:
+    print("ERROR!!!")
+
+    
+
+#########################################
+# USE BELOW AS TEMPLATE to create dropdowns, then backspace each line to get the gigantic 1 line mess 
+##########################################
+####raycast.argument1 { "type": "dropdown", "placeholder": "sel config", "optional": true, "data": [
+# {"title": "aliases", "value": 1}, 
+# {"title": "ahk", "value": 2}, 
+# {"title": "glaze", "value": 3},
+# {"title": "flexprompt", "value": 4}, 
+# {"title": "fzf", "value": 5}, 
+# {"title": "keybVis", "value": 6},
+# {"title": "ssotrefs", "value": "7"}, 
+# {"title": "madden", "value": "8"} ] }
+
+# {"title": "aliases", "value": 1},         A
+# {"title": "ahk", "value": 2},             H
+# {"title": "glaze", "value": 3},           G
+# {"title": "flexprompt", "value": 4},      P
+# {"title": "fzf", "value": 5},             F   
+# {"title": "keybVisualizer", "value": 6},         K
+# {"title": "ssot_refs", "value": "7"},      R
+# {"title": "madden", "value": "8"} ] }     M
---
+++ win/config-dropdown2.sh
@@ -0,0 +1,154 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Config Dropdown2
+# @raycast.mode fullOutput
+# @raycast.icon 🤖
+# @raycast.packageName Developer Utils
+# @raycast.description trying again
+
+# @raycast.argument1 { "type": "dropdown", "placeholder": "sel file", "optional": true, "data": [{"title": "refsmd", "value": "C:'\dsfddfg"}, {"title": "BBBB", "value": "BBBBBBBBBBBB"}, {"title": "aerospace", "value": "/Users/alexking/amk/tools/GIT_STUFF/niche_side_quests_repo/mac/aerospace.toml"}, {"title": "madden", "value": "/Users/alexking/Desktop/DESKTOP_SHELF/2024_and_prior/TEMPORARY_FOLDER_TO_CLEAR_DESKTOP_UNTIL_INTERNSHIP_SEARCHDONE/ssot-madden.txt"}] }
+
+
+
+
+#### @raycast.argument1 { "type": "dropdown", "placeholder": "sel file", "optional": true, "data": [{"title": "refsmd", "value": "C:\Users\aking\Downloads\cmder\config\user_aliases.cmd"}, {"title": "keybVis", "value": "C:\Users\aking\Downloads\cmder\config\user_aliases.cmd"}, {"title": "aerospace", "value": "C:\Users\aking\Downloads\cmder\config\user_aliases.cmd"}, {"title": "madden", "value": "C:\Users\aking\Downloads\cmder\config\user_aliases.cmd"}, {"title": "madden222", "value": "C:\Users\aking\Downloads\cmder\config\user_aliases.cmd"}, {"title": "madden333", "value": "/Users/alexking/Desktop/DESKTOP_SHELF/2024_and_prior/ssot-madden444.txt"} ] }
+
+#### @raycast.argument1 { "type": "dropdown", "placeholder": "sel file", "optional": true, "data": [{"title": "refsmd", "value": "/Users/alexking/amk/tools/GIT_STUFF/niche_side_quests_repo/_universal/ssot-refs.md"}, {"title": "keybVis", "value": "/Users/alexking/amk/tools/GIT_STUFF/niche_side_quests_repo/_universal/keyboard-shortcuts.html"}, {"title": "aerospace", "value": "/Users/alexking/amk/tools/GIT_STUFF/niche_side_quests_repo/mac/aerospace.toml"}, {"title": "madden", "value": "/Users/alexking/Desktop/DESKTOP_SHELF/2024_and_prior/TEMPORARY_FOLDER_TO_CLEAR_DESKTOP_UNTIL_INTERNSHIP_SEARCHDONE/ssot-madden.txt"}] }
+
+
+## @raycast.argument1 { "type": "dropdown", "placeholder": "sel file", "optional": true, "data": [{"title": "refsmd", "value": "/Users/alexking/amk/tools/GIT_STUFF/niche_side_quests_repo/_universal/ssot-refs.md"}, {"title": "keybVis", "value": "/Users/alexking/amk/tools/GIT_STUFF/niche_side_quests_repo/_universal/keyboard-shortcuts.html"}, {"title": "aerospace", "value": "/Users/alexking/amk/tools/GIT_STUFF/niche_side_quests_repo/mac/aerospace.toml"}, {"title": "madden", "value": "/Users/alexking/Desktop/DESKTOP_SHELF/2024_and_prior/ssot-madden.txt"}, {"title": "madden222", "value": "/Users/alexking/Desktop/DESKTOP_SHELF/2024_and_prior/ssot-madden333.txt"}, {"title": "madden333", "value": "/Users/alexking/Desktop/DESKTOP_SHELF/2024_and_prior/ssot-madden444.txt"} ] }
+
+### @raycast.argument1 { "type": "dropdown", "placeholder": "sel file", "optional": true, "data": [{"title": "aliases", "value": "C:\Users\aking\Downloads\cmder\config\user_aliases.cmd"}, {"title": "ahk", "value": "C:\Users\aking\AppData\Local\Programs\AutoHotkey\UX\king_app_switcher1.ahk"}, {"title": "ahk2", "value": "C:\Users\aking\AppData\Local\Programs\AutoHotckey\UX\king_app_switcher12.ahk"}, {"title": "glaze", "value": "C:\Users\aking\.glzr\glazewm\config.yaml"}] }
+### @raycast.argument1 { "type": "dropdown", "placeholder": "sel file", "optional": true, "data": [{"title": "refsmd", "value": "/Users/alexking/amk/tools/GIT_STUFF/niche_side_quests_repo/_universal/ssot-refs.md"}, {"title": "keybVis", "value": "/Users/alexking/amk/tools/GIT_STUFF/niche_side_quests_repo/_universal/keyboard-shortcuts.html"}, {"title": "aerospace", "value": "/Users/alexking/amk/tools/GIT_STUFF/niche_side_quests_repo/mac/aerospace.toml"}, {"title": "madden", "value": "/Users/alexking/Desktop/DESKTOP_SHELF/2024_and_prior/ssot-madden.txt"}, {"title": "madden222", "value": "/Users/alexking/Desktop/DESKTOP_SHELF/2024_and_prior/ssot-madden333.txt"}, {"title": "madden333", "value": "/Users/alexking/Desktop/DESKTOP_SHELF/2024_and_prior/ssot-madden444.txt"} ] }
+
+
+
+
+#### @raycast.argument1 { "type": "dropdown", "placeholder": "sel file", "optional": true, "data": [{"title": "refsmd", "value": "/Users/alexking/amk/tools/GIT_STUFF/niche_side_quests_repo/_universal/ssot-refs.md"}, {"title": "keybVis", "value": "/Users/alexking/amk/tools/GIT_STUFF/niche_side_quests_repo/_universal/keyboard-shortcuts.html"}, {"title": "aerospace", "value": "/Users/alexking/amk/tools/GIT_STUFF/niche_side_quests_repo/mac/aerospace.toml"}, {"title": "madden", "value": "/Users/alexking/Desktop/DESKTOP_SHELF/2024_and_prior/ssot-madden.txt"}, {"title": "madden222", "value": "/Users/alexking/Desktop/DESKTOP_SHELF/2024_and_prior/ssot-madden333.txt"}, {"title": "madden333", "value": "/Users/alexking/Desktop/DESKTOP_SHELF/2024_and_prior/ssot-madden444.txt"} ] }
+
+
+##### @raycast.argument1 { "type": "dropdown", "placeholder": "sel config", "optional": true, "data": [{"title": "aliases", "value": "C:\Users\aking\Downloads\cmder\config\user_aliases.cmd"}, {"title": "ahk", "value": "C:\Users\aking\AppData\Local\Programs\AutoHotkey\UX\king_app_switcher1.ahk"},{"title": "glaze", "value": "C:\Users\aking\.glzr\glazewm\config.yaml"}, {"title": "flexprompt", "value": "C:\Users\aking\Downloads\cmder\config\flexprompt_config.lua"}, {"title": "fzf", "value": "C:\Users\aking\Downloads\cmder\vendor\clink\clink_gizmos\fzf.lua"}, {"title": "keybVis", "value": "D:\git\niche_side_quests_repo\_universal\keyboard-shortcuts.html"}, {"title": "ssot-refs", "value": "D:\git\niche_side_quests_repo\_universal\ssot-refs.md"}, {"title": "madden", "value": "INSERT_HERE"}] }
+#### @raycast.argument1 { "type": "dropdown", "placeholder": "sel config", "optional": true, "data": [
+# {"title": "aliases", "value": "C:\Users\aking\Downloads\cmder\config\user_aliases.cmd"}, 
+# {"title": "ahk", "value": "C:\Users\aking\AppData\Local\Programs\AutoHotkey\UX\king_app_switcher1.ahk"},
+# {"title": "glaze", "value": "C:\Users\aking\.glzr\glazewm\config.yaml"}, 
+# {"title": "flexprompt", "value": "C:\Users\aking\Downloads\cmder\config\flexprompt_config.lua"}, 
+# {"title": "fzf", "value": "C:\Users\aking\Downloads\cmder\vendor\clink\clink_gizmos\fzf.lua"}, 
+# {"title": "keybVis", "value": "D:\git\niche_side_quests_repo\_universal\keyboard-shortcuts.html"}, 
+# {"title": "ssot-refs", "value": "D:\git\niche_side_quests_repo\_universal\ssot-refs.md"}, 
+# {"title": "madden", "value": "INSERT_HERE"}
+# ] }
+
+
+#### @raycast.argument1 { "type": "text", "placeholder": "Placeholder" }
+#### @raycast.argument1 { "type": "dropdown", "placeholder": "sel file", "optional": true, "data": [{"title": "refsmd", "value": "/Users/alexking/amk/tools/GIT_STUFF/niche_side_quests_repo/_universal/ssot-refs.md"}, {"title": "keybVis", "value": "/Users/alexking/amk/tools/GIT_STUFF/niche_side_quests_repo/_universal/keyboard-shortcuts.html"}, {"title": "aerospace", "value": "/Users/alexking/amk/tools/GIT_STUFF/niche_side_quests_repo/mac/aerospace.toml"}, {"title": "madden", "value": "/Users/alexking/Desktop/DESKTOP_SHELF/2024_and_prior/TEMPORARY_FOLDER_TO_CLEAR_DESKTOP_UNTIL_INTERNSHIP_SEARCHDONE/ssot-madden.txt"}] }
+
+
+
+
+##### PATHS OF ALL THE CONFIGS I WANT IN THIS DROPDOWN ... 
+# C:\Users\aking\Downloads\cmder\config\user_aliases.cmd
+# C:\Users\aking\.glzr\glazewm\config.yaml
+# C:\Users\aking\AppData\Local\Programs\AutoHotkey\UX\king_app_switcher1.ahk
+# C:\Users\aking\Downloads\cmder\config\flexprompt_config.lua
+# C:\Users\aking\Downloads\cmder\vendor\clink\clink_gizmos\fzf.lua
+# code D:\git\niche_side_quests_repo\_universal\keyboard-shortcuts.html
+# D:\git\niche_side_quests_repo\_universal\ssot-refs.md
+#
+#
+
+
+
+# Documentation:
+# @raycast.author alexmking921
+# @raycast.authorURL https://raycast.com/alexmking921
+
+echo "Hello World! Argument1 value: $1"
+
+
+
+
+
+####### @raycast.argument1 { "type": "dropdown", "placeholder": "sel config", "optional": true, "data": [{"title": "aliases", "value": "1"}, {"title": "ahk", "value": "2"},{"title": "glaze", "value": "3"}, {"title": "flexprompt", "4"}, {"title": "fzf", "value": "5"}, {"title": "keybVis", "value": "6"}, {"title": "ssotrefs", "value": "7"}, {"title": "madden", "value": "8"}] }
+
+##### @raycast.argument1 { "type": "dropdown", "placeholder": "sel file", "optional": true, "data": [{"title": "refsmd", "value": "C:/dfg"}, {"title": "BBBB", "value": "BBBBBBBBBBBB"}, {"title": "aerospace", "value": "/Users/alexking/amk/tools/GIT_STUFF/niche_side_quests_repo/mac/aerospace.toml"}, {"title": "madden", "value": "/Users/alexking/Desktop/DESKTOP_SHELF/2024_and_prior/TEMPORARY_FOLDER_TO_CLEAR_DESKTOP_UNTIL_INTERNSHIP_SEARCHDONE/ssot-madden.txt"}] }
+
+
+
+
+#### @raycast.argument1 { "type": "dropdown", "placeholder": "sel file", "optional": true, "data": [{"title": "refsmd", "value": "C:\Users\aking\Downloads\cmder\config\user_aliases.cmd"}, {"title": "keybVis", "value": "C:\Users\aking\Downloads\cmder\config\user_aliases.cmd"}, {"title": "aerospace", "value": "C:\Users\aking\Downloads\cmder\config\user_aliases.cmd"}, {"title": "madden", "value": "C:\Users\aking\Downloads\cmder\config\user_aliases.cmd"}, {"title": "madden222", "value": "C:\Users\aking\Downloads\cmder\config\user_aliases.cmd"}, {"title": "madden333", "value": "/Users/alexking/Desktop/DESKTOP_SHELF/2024_and_prior/ssot-madden444.txt"} ] }
+
+#### @raycast.argument1 { "type": "dropdown", "placeholder": "sel file", "optional": true, "data": [{"title": "refsmd", "value": "/Users/alexking/amk/tools/GIT_STUFF/niche_side_quests_repo/_universal/ssot-refs.md"}, {"title": "keybVis", "value": "/Users/alexking/amk/tools/GIT_STUFF/niche_side_quests_repo/_universal/keyboard-shortcuts.html"}, {"title": "aerospace", "value": "/Users/alexking/amk/tools/GIT_STUFF/niche_side_quests_repo/mac/aerospace.toml"}, {"title": "madden", "value": "/Users/alexking/Desktop/DESKTOP_SHELF/2024_and_prior/TEMPORARY_FOLDER_TO_CLEAR_DESKTOP_UNTIL_INTERNSHIP_SEARCHDONE/ssot-madden.txt"}] }
+
+
+## @raycast.argument1 { "type": "dropdown", "placeholder": "sel file", "optional": true, "data": [{"title": "refsmd", "value": "/Users/alexking/amk/tools/GIT_STUFF/niche_side_quests_repo/_universal/ssot-refs.md"}, {"title": "keybVis", "value": "/Users/alexking/amk/tools/GIT_STUFF/niche_side_quests_repo/_universal/keyboard-shortcuts.html"}, {"title": "aerospace", "value": "/Users/alexking/amk/tools/GIT_STUFF/niche_side_quests_repo/mac/aerospace.toml"}, {"title": "madden", "value": "/Users/alexking/Desktop/DESKTOP_SHELF/2024_and_prior/ssot-madden.txt"}, {"title": "madden222", "value": "/Users/alexking/Desktop/DESKTOP_SHELF/2024_and_prior/ssot-madden333.txt"}, {"title": "madden333", "value": "/Users/alexking/Desktop/DESKTOP_SHELF/2024_and_prior/ssot-madden444.txt"} ] }
+
+### @raycast.argument1 { "type": "dropdown", "placeholder": "sel file", "optional": true, "data": [{"title": "aliases", "value": "C:\Users\aking\Downloads\cmder\config\user_aliases.cmd"}, {"title": "ahk", "value": "C:\Users\aking\AppData\Local\Programs\AutoHotkey\UX\king_app_switcher1.ahk"}, {"title": "ahk2", "value": "C:\Users\aking\AppData\Local\Programs\AutoHotckey\UX\king_app_switcher12.ahk"}, {"title": "glaze", "value": "C:\Users\aking\.glzr\glazewm\config.yaml"}] }
+### @raycast.argument1 { "type": "dropdown", "placeholder": "sel file", "optional": true, "data": [{"title": "refsmd", "value": "/Users/alexking/amk/tools/GIT_STUFF/niche_side_quests_repo/_universal/ssot-refs.md"}, {"title": "keybVis", "value": "/Users/alexking/amk/tools/GIT_STUFF/niche_side_quests_repo/_universal/keyboard-shortcuts.html"}, {"title": "aerospace", "value": "/Users/alexking/amk/tools/GIT_STUFF/niche_side_quests_repo/mac/aerospace.toml"}, {"title": "madden", "value": "/Users/alexking/Desktop/DESKTOP_SHELF/2024_and_prior/ssot-madden.txt"}, {"title": "madden222", "value": "/Users/alexking/Desktop/DESKTOP_SHELF/2024_and_prior/ssot-madden333.txt"}, {"title": "madden333", "value": "/Users/alexking/Desktop/DESKTOP_SHELF/2024_and_prior/ssot-madden444.txt"} ] }
+
+
+
+
+#### @raycast.argument1 { "type": "dropdown", "placeholder": "sel file", "optional": true, "data": [{"title": "refsmd", "value": "/Users/alexking/amk/tools/GIT_STUFF/niche_side_quests_repo/_universal/ssot-refs.md"}, {"title": "keybVis", "value": "/Users/alexking/amk/tools/GIT_STUFF/niche_side_quests_repo/_universal/keyboard-shortcuts.html"}, {"title": "aerospace", "value": "/Users/alexking/amk/tools/GIT_STUFF/niche_side_quests_repo/mac/aerospace.toml"}, {"title": "madden", "value": "/Users/alexking/Desktop/DESKTOP_SHELF/2024_and_prior/ssot-madden.txt"}, {"title": "madden222", "value": "/Users/alexking/Desktop/DESKTOP_SHELF/2024_and_prior/ssot-madden333.txt"}, {"title": "madden333", "value": "/Users/alexking/Desktop/DESKTOP_SHELF/2024_and_prior/ssot-madden444.txt"} ] }
+
+
+##### @raycast.argument1 { "type": "dropdown", "placeholder": "sel config", "optional": true, "data": [{"title": "aliases", "value": "C:\Users\aking\Downloads\cmder\config\user_aliases.cmd"}, {"title": "ahk", "value": "C:\Users\aking\AppData\Local\Programs\AutoHotkey\UX\king_app_switcher1.ahk"},{"title": "glaze", "value": "C:\Users\aking\.glzr\glazewm\config.yaml"}, {"title": "flexprompt", "value": "C:\Users\aking\Downloads\cmder\config\flexprompt_config.lua"}, {"title": "fzf", "value": "C:\Users\aking\Downloads\cmder\vendor\clink\clink_gizmos\fzf.lua"}, {"title": "keybVis", "value": "D:\git\niche_side_quests_repo\_universal\keyboard-shortcuts.html"}, {"title": "ssot-refs", "value": "D:\git\niche_side_quests_repo\_universal\ssot-refs.md"}, {"title": "madden", "value": "INSERT_HERE"}] }
+#### @raycast.argument1 { "type": "dropdown", "placeholder": "sel config", "optional": true, "data": [
+# {"title": "aliases", "value": "C:\Users\aking\Downloads\cmder\config\user_aliases.cmd"}, 
+# {"title": "ahk", "value": "C:\Users\aking\AppData\Local\Programs\AutoHotkey\UX\king_app_switcher1.ahk"},
+# {"title": "glaze", "value": "C:\Users\aking\.glzr\glazewm\config.yaml"}, 
+# {"title": "flexprompt", "value": "C:\Users\aking\Downloads\cmder\config\flexprompt_config.lua"}, 
+# {"title": "fzf", "value": "C:\Users\aking\Downloads\cmder\vendor\clink\clink_gizmos\fzf.lua"}, 
+# {"title": "keybVis", "value": "D:\git\niche_side_quests_repo\_universal\keyboard-shortcuts.html"}, 
+# {"title": "ssot-refs", "value": "D:\git\niche_side_quests_repo\_universal\ssot-refs.md"}, 
+# {"title": "madden", "value": "INSERT_HERE"}
+# ] }
+
+
+#### @raycast.argument1 { "type": "dropdown", "placeholder": "sel config", "optional": true, "data": [
+# {"title": "aliases", "value": "1"}, 
+# {"title": "ahk", "value": "2"},
+# {"title": "glaze", "value": "3"}, 
+# {"title": "flexprompt", "4"}, 
+# {"title": "fzf", "value": "5"}, 
+# {"title": "keybVis", "value": "6"}, 
+# {"title": "ssot-refs", "value": "7"}, 
+# {"title": "madden", "value": "8"}
+# ] }
+
+
+#### @raycast.argument1 { "type": "dropdown", "placeholder": "sel config", "optional": true, "data": [{"title": "aliases", "value": "1"}, {"title": "ahk", "value": "2"},{"title": "glaze", "value": "3"}, {"title": "flexprompt", "4"}, {"title": "fzf", "value": "5"}, {"title": "keybVis", "value": "6"}, {"title": "ssot-refs", "value": "7"}, {"title": "madden", "value": "8"}] }
+
+
+#### @raycast.argument1 { "type": "text", "placeholder": "Placeholder" }
+#### @raycast.argument1 { "type": "dropdown", "placeholder": "sel file", "optional": true, "data": [{"title": "refsmd", "value": "/Users/alexking/amk/tools/GIT_STUFF/niche_side_quests_repo/_universal/ssot-refs.md"}, {"title": "keybVis", "value": "/Users/alexking/amk/tools/GIT_STUFF/niche_side_quests_repo/_universal/keyboard-shortcuts.html"}, {"title": "aerospace", "value": "/Users/alexking/amk/tools/GIT_STUFF/niche_side_quests_repo/mac/aerospace.toml"}, {"title": "madden", "value": "/Users/alexking/Desktop/DESKTOP_SHELF/2024_and_prior/TEMPORARY_FOLDER_TO_CLEAR_DESKTOP_UNTIL_INTERNSHIP_SEARCHDONE/ssot-madden.txt"}] }
+
+
+
+
+##### PATHS OF ALL THE CONFIGS I WANT IN THIS DROPDOWN ... 
+# C:\Users\aking\Downloads\cmder\config\user_aliases.cmd
+# C:\Users\aking\.glzr\glazewm\config.yaml
+# C:\Users\aking\AppData\Local\Programs\AutoHotkey\UX\king_app_switcher1.ahk
+# C:\Users\aking\Downloads\cmder\config\flexprompt_config.lua
+# C:\Users\aking\Downloads\cmder\vendor\clink\clink_gizmos\fzf.lua
+# code D:\git\niche_side_quests_repo\_universal\keyboard-shortcuts.html
+# D:\git\niche_side_quests_repo\_universal\ssot-refs.md
+#
+#
+
+
+
+
+
+
+
+
+
+
+
+
+#####sdfasdf raycast.argument1 { "type": "dropdown", "placeholder": "sel file", "optional": true, "data": [{"title": "refsmd", "value": "1"}, {"title": "keybVis", "value": "2"}, {"title": "aerospace", "value": "3"}, {"title": "madden", "value": "4"}, {"title": "madden222", "value": "5"}, {"title": "madden333", "value": "6"} ] }
+
