794207a4b48ed2aa9e9ed9908122afd09b0561e0
alexmking <alexking921@yahoo.com>
2026-05-20 11:35:25 -0500

    VSCODE_keyb_VIZ3

--- mac/edit-vskeybvis-mac.sh
+++ mac/edit-vskeybvis-mac.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
 # @raycast.schemaVersion 1
-# @raycast.title Edit VSKeybVis
+# @raycast.title Edit KeybVis
 # @raycast.mode compact
-# @raycast.description Open VSCode's keyboard visualization file in VS Code for quick edits
+# @raycast.description Open keyboard visualization file in VS Code for quick edits
 # @raycast.packageName Developer Utils
 # @raycast.icon 📝
 
 # FILE="/usr/local/bin/alexScripts/raycast_scripts/ssot-refs.txt"
 # FILE="/Users/alexking/local_docs/GIT_STUFF/niche_side_quests_repo/_universal/ssot-refs.md"
-# FILE="/Users/alexking/amk/tools/GIT_STUFF/niche_side_quests_repo/_universal/keyboard-shortcuts.html"
-FILE="/Users/alexking/amk/tools/GIT_STUFF/niche_side_quests_repo/_universal/vscode-shortcuts.html"
+FILE="/Users/alexking/amk/tools/GIT_STUFF/niche_side_quests_repo/_universal/keyboard-shortcuts.html"
 # FILE="/Users/alexking/Desktop/DESKTOP_SHELF/2024_and_prior/TEMPORARY_FOLDER_TO_CLEAR_DESKTOP_UNTIL_INTERNSHIP_SEARCHDONE/ssot-madden.txt"
 # FILE="/Users/alexking/amk/tools/GIT_STUFF/niche_side_quests_repo/mac/.zshrc"
 # FILE="/Users/alexking/amk/tools/GIT_STUFF/niche_side_quests_repo/mac/aerospace.toml"
@@ -26,4 +25,4 @@ code "$FILE"
 # Open in VS Code (TRYING TO FIX THE SLOWNESS w this same-window flag, idk)
 # code --reuse-window "$FILE"
 
-/Users/alexking/amk/tools/GIT_STUFF/niche_side_quests_repo
+
