dbb19f8ce3e8e44c8d453add5b69a489b6e017e2
King, Alex <alex.king@osii.com>
2026-05-20 19:26:06 -0500

    updates3

---
+++ win/edit-scratchpaper.sh
@@ -0,0 +1,22 @@
+# !/bin/bash
+
+# @raycast.schemaVersion 1
+# @raycast.title Edit scratchpaper
+# @raycast.mode silent
+# @raycast.description Open scratchpaper file in vscode
+# @raycast.packageName Developer Utils
+# @raycast.icon 📝
+
+# FILE="/usr/local/bin/alexScripts/raycast_scripts/ssot-refs.txt"
+# FILE="/Users/alexking/local_docs/GIT_STUFF/niche_side_quests_repo/_universal/ssot-refs.md"
+# FILE="D:\git\niche_side_quests_repo\_universal\keyboard-shortcuts.html"
+FILE="C:\Users\aking\amk\tools\alex_scripts\scr-scratch_paper_area.py"
+
+# Check if file exists
+if [ ! -f "$FILE" ]; then
+  echo "File not found: $FILE"
+  exit 1
+fi
+
+# Open in VS Code
+code "$FILE"
