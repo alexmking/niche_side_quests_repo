ea40e19a93dfa08014aaf1565ff2b34283eef4e5
alexmking <alexking921@yahoo.com>
2026-07-01 19:40:12 -0500

    more devland terms added (2/3 done roughly)3

---
+++ mac/launchMainApps.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Launch Main Apps
+# @raycast.mode fullOutput
+
+# Optional parameters:
+# @raycast.icon 🚀
+# @raycast.packageName Developer Utils
+
+# Documentation:
+# @raycast.description launch all my main apps (iTerm, Teams, Spotify, etc.) in one go
+# @raycast.author alexmking921
+# @raycast.authorURL https://raycast.com/alexmking921
+
+# print("Hello World!")
+
+
+import os
+# import sys
+
+# ACTUAL FULL PATH: /Users/alexking/amk/tools/GIT_STUFF/niche_side_quests_repo/_universal/scr-leet_morning_random.py easymed group1 onlytopproblems
+# os.system("/Users/alexking/amk/tools/GIT_STUFF/niche_side_quests_repo/_universal/scr-leet_morning_random.py easymed group1 onlytopproblems")
+# os.system("/Users/alexking/amk/tools/GIT_STUFF/niche_side_quests_repo/_universal/scr-leet_morning_random.py easymed group1 onlytopproblems")
+
+os.system("/Users/alexking/amk/tools/alexScripts/scr-launch_my_main_apps.py")
+
+
+#### TRYING TO USE MY ENV-VAR for the PATH DEFINTION, but kept hitting brick walls so abandoned it...
+# os.system("/Users/alexking/amk/tools/GIT_STUFF/niche_side_quests_repo/_universal/scr-leet_morning_random.py easymed group1 onlytopproblems")
+# os.system("$NICHE_REPO_DIR/_universal/scr-leet_morning_random.py easymed group1 onlytopproblems")
+# os.system(f"{os.environ['NICHE_REPO_DIR']}/_universal/scr-leet_morning_random.py easymed group1 onlytopproblems")
+# print(os.environ['NICHE_REPO_DIR'] + "/_universal/scr-leet_morning_random.py easymed group1 onlytopproblems")
+# print(os.environ['HOME'] + "/tools/GIT_STUFF/niche_side_quests_repo/_universal/scr-leet_morning_random.py easymed group1 onlytopproblems")
+
