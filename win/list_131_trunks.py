e870637e866d66398c353831f87f0271229693b1
King, Alex <alex.king@osii.com>
2026-08-21 10:35:53 -0500

    misc additions

---
+++ win/list_131_trunks.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title list 13.1 trunks
+# @raycast.mode fullOutput
+
+# Optional parameters:
+# @raycast.icon ⚡
+# @raycast.packageName Developer Utils
+
+# Documentation:
+# @raycast.description list 13.1 trunks (same as list_13.1_trunks_amk terminal alias)
+# @raycast.author alexmking921
+# @raycast.authorURL https://raycast.com/alexmking921
+
+# print("Hello World!")
+
+
+import os
+# import sys
+
+# ACTUAL FULL PATH: /Users/alexking/amk/tools/GIT_STUFF/niche_side_quests_repo/_universal/scr-leet_morning_random.py easymed group1 onlytopproblems
+os.system("C:\\Users\\aking\\amk\\tools\\alex_scripts\\list_13.1_trunks_amk.py")
+
+
+#### TRYING TO USE MY ENV-VAR for the PATH DEFINTION, but kept hitting brick walls so abandoned it...
+# os.system("/Users/alexking/amk/tools/GIT_STUFF/niche_side_quests_repo/_universal/scr-leet_morning_random.py easymed group1 onlytopproblems")
+# os.system("$NICHE_REPO_DIR/_universal/scr-leet_morning_random.py easymed group1 onlytopproblems")
+# os.system(f"{os.environ['NICHE_REPO_DIR']}/_universal/scr-leet_morning_random.py easymed group1 onlytopproblems")
+# print(os.environ['NICHE_REPO_DIR'] + "/_universal/scr-leet_morning_random.py easymed group1 onlytopproblems")
+# print(os.environ['HOME'] + "/tools/GIT_STUFF/niche_side_quests_repo/_universal/scr-leet_morning_random.py easymed group1 onlytopproblems")
+
